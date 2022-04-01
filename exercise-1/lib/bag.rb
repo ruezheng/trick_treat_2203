23912a64ee8dc066b8f5888389dac8d9cb5a3232
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-01 10:13:38 -0600

    Feat: bag_spec

---
+++ exercise-1/lib/bag.rb
@@ -0,0 +1,27 @@
+class Bag
+  attr_reader :candies
+
+  def initialize
+    @candies = []
+  end
+
+  def empty?
+    @candies.length == 0
+  end
+
+  def count
+    @candies.count
+  end
+
+  def add_candy(candy)
+    @candies << candy
+  end
+
+  def contains?(type)
+    @candies.find do |candy|
+      if candy.type == type
+      end
+    end
+    return false
+  end
+end
